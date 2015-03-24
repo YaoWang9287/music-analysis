fe40293b10d9864c45ea4d5baa981c945bc0e854
kevinwan1991 <kevinwan1991@hotmail.com>
2015-03-24 00:53:47 -0400

    modify

--- src/main/java/org/music/analysis/db_test.java
+++ src/main/java/org/music/analysis/db_test.java
@@ -7,9 +7,6 @@ import java.sql.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-/**
- * Servlet implementation class Home
- */
 public class db_test extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -25,52 +22,55 @@ public class db_test extends HttpServlet {
 		
 		Statement stmt = null;
 		Connection conn = null;
+		PrintWriter out = response.getWriter();
+		out.println("<h1>"+"java-db test"+"</h1>");
 		// Register JDBC driver
-        try {
+	        try {
 			Class.forName("com.mysql.jdbc.Driver");
-	        // Open a connection
-	        conn = DriverManager.getConnection(DB_URL,USER,PASS);
+	        	// Open a connection
+		        conn = DriverManager.getConnection(DB_URL,USER,PASS);
 	
-	        // Execute SQL query
-	        stmt = conn.createStatement();
-	        String sql;
-	        sql = "SELECT * FROM Albums Limit 10";
-	        ResultSet rs = stmt.executeQuery(sql);
+	       		// Execute SQL query
+	        	stmt = conn.createStatement();
+	        	String sql;
+	      		sql = "SELECT * FROM Albums Limit 10";
+	        	ResultSet rs = stmt.executeQuery(sql);
 	        
-	        response.setContentType("text/html");
+	        	response.setContentType("text/html");
 	        
-	        PrintWriter out = response.getWriter();
-	        String title = "Database Album";
-	        String docType =
-	          "<!doctype html public \"-//w3c//dtd html 4.0 " +
-	           "transitional//en\">\n";
-	           out.println(docType +
-	           "<html>\n" +
-	           "<head><title>" + title + "</title></head>\n" +
-	           "<body bgcolor=\"#f0f0f0\">\n" +
-	           "<h1 align=\"center\">" + title + "</h1>\n");
-	        while(rs.next()){
-	        	//Retrieve by column name	            
-	            String aName  = rs.getString("albumName");
-	        	String aCountry = rs.getString("albumCountry");
-	            String aLanguage = rs.getString("albumLanguage");
-	           	String barcode = rs.getString("albumBarCode");
-	            String sId = rs.getString("songId");
-	            int aId = rs.getInt("albumId");
+	        	//PrintWriter out = response.getWriter();
+	        	String title = "Database Album";
+//	        	String docType =
+//	          		"<!doctype html public \"-//w3c//dtd html 4.0 " +
+//	           		"transitional//en\">\n";
+//	           		out.println(docType +
+//	           		"<html>\n" +
+//	           		"<head><title>" + title + "</title></head>\n" +
+//	           		"<body bgcolor=\"#f0f0f0\">\n" +
+//	           		"<h1 align=\"center\">" + title + "</h1>\n");
+		      	while(rs.next()){
+	        		//Retrieve by column name	            
+	            		String aName  = rs.getString("albumName");
+	        		String aCountry = rs.getString("albumCountry");
+	            		String aLanguage = rs.getString("albumLanguage");
+	           		String barcode = rs.getString("albumBarCode");
+	            		String sId = rs.getString("songId");
+	            		int aId = rs.getInt("albumId");
 
-	            //Display values
-	            out.println("albumName: " + aName + "<br>");
-	            out.println(", albumCountry: " + aCountry + "<br>");
-	            out.println(", albumLanguage: " + aLanguage + "<br>");
-	            out.println(", albumId: " + aId + "<br>");
-	            out.println(", songId: " + sId + "<br>");
-	        }
+	            		//Display values
+	            		out.println("<h1>"+"albumName: " + aName + "<h1>");
+	            		out.println("<h1>"+"albumCountry: " + aCountry + "<br>");
+	            		out.println("<h1>"+"albumLanguage: " + aLanguage + "<br>");
+	            		out.println("<h1>"+"albumId: " + aId + "<br>");
+	            		out.println("<h1>"+"songId: " + sId + "<br>");
+	        	}	
 		
-        } catch (ClassNotFoundException e) {
+        	} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
+			out.println("<h1>"+"sql failed"+"</h1>");
 			e.printStackTrace();
 		}finally{
 	         //finally block used to close resources
