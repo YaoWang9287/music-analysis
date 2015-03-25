a2c67c4f38ba4a0739f9c0ddeaf1cb24bfd7f38e
kevinwan1991 <kevinwan1991@hotmail.com>
2015-03-25 17:13:52 -0400

    MVC established

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source /home/ec2-user/.bash_profile
 mvn compile
 mvn package
-sudo cp /home/ec2-user/music-analysis/musicA/target/musicA-1.0-SNAPSHOT.war /usr/local/tomcat7/webapps/musicA-1.0-SNAPSHOT.war   
+sudo cp /home/ec2-user/music-analysis/musicA/target/musicA-1.0-SNAPSHOT.war /home/ec2-user/opt/apache-tomcat-7.0.59/webapps/musicA-1.0-SNAPSHOT.war  # /usr/local/tomcat7/ 
